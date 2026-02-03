c08f6a9cb98313d2beb319d5dcbe5b7b35e0cfe9
mikegg4545 <42780320+mikegg4545@users.noreply.github.com>
2026-02-03 14:42:41 -0700

    Add createElement, append child, stylesheet link and css styling

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("My code is running");
+
+let input = document.createElement("input");
+input.type = "text";
+input.placeholder = "Task tracking?...";
+
+document.body.appendChild(input);
