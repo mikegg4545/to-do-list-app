25b91726bfd9a92f26ce03928b623aafe5cbd368
mikegg4545 <42780320+mikegg4545@users.noreply.github.com>
2026-02-03 21:11:17 -0700

    Update input, button and css

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 console.log("My code is running");
 
-let input = document.createElement("input");
-input.type = "text";
-input.placeholder = "Task tracking?...";
+// let input = document.createElement("input");
+// input.type = "text";
+// input.placeholder = "Task tracking?...";
 
-document.body.appendChild(input);
+// document.body.appendChild(input);
+
+// const button = document.createElement("button");
+// button.textContent = "Add Task ☕️☀️👍";
+
+// document.body.appendChild(button);
+
+function addTask() {}
